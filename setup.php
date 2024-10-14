3ab087ab1c2bacfd1ddfabbc907fd572078b7bc8
Antoine Lemarchand <antoine.lemarchand@itsm-ng.com>
2024-10-14 10:54:40 +0200

    bump version

--- setup.php
+++ setup.php
@@ -22,7 +22,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_CMDB_VERSION', '2.3.0');
+define('PLUGIN_CMDB_VERSION', '3.0.0');
 
 if (!defined("PLUGINCMDB_DIR")) {
    define("PLUGINCMDB_DIR", GLPI_ROOT . "/plugins/cmdb");
